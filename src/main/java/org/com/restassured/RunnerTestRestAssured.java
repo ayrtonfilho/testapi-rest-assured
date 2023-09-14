cb8d44f9b83c1638fe68d4904c469d1ea6c8444f
Airton de Sousa Martins Filho <ayrtonfilho12345@gmail.com>
2023-09-13 22:44:24 -0300

    method test created

--- src/main/java/org/com/restassured/RunnerTestRestAssured.java
+++ src/main/java/org/com/restassured/RunnerTestRestAssured.java
@@ -3,20 +3,22 @@ package org.com.restassured;
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
-import io.restassured.response.ValidatableResponse;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 
 public class RunnerTestRestAssured {
 
     @Test
-    public static void main(String[] args) {
+    public void testMainMethod() {
         Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me:80/ola");
+        System.out.println(response.getStatusCode());
+        System.out.println(response.getBody().asString());
 
-        System.out.println(response.getBody().asString().equals("Ola Mundo!"));
-        System.out.println(response.statusCode() == 200);
+        Assertions.assertEquals(200, response.statusCode());
+    }
 
-        ValidatableResponse validacao = response.then();
-        validacao.statusCode(201);
-
+    public static void main(String[] args) {
+        System.out.println("Units tests created!");
     }
 }
